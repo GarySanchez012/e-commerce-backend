d18485af3a99f9637125ab39e05b586de60aecdc
arroyhondo <garysanchez012@gmail.com>
2021-10-15 14:28:38 -0400

    finished the RESTful API routes and their CRUD methods

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -1,6 +1,6 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
-const Category = require('./Category');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+const Category = require("./Category");
 
 class Product extends Model {}
 
@@ -10,41 +10,41 @@ Product.init(
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
-      autoIncrement: true
+      autoIncrement: true,
     },
     product_name: {
-      type:DataTypes.STRING,
-      allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     price: {
       type: DataTypes.DECIMAL,
-      allowNull:false,
+      allowNull: false,
       validate: {
-        isDecimal: true
-      }
+        isDecimal: true,
+      },
     },
-    stock : {
+    stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
       defaultValue: 10,
       validate: {
-        isNumeric: true
+        isNumeric: true,
       },
       category_id: {
         type: DataTypes.INTEGER,
         references: {
           model: Category,
-          key: 'id'
-        }
-      }
-    }
+          key: "id",
+        },
+      },
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product',
+    modelName: "product",
   }
 );
 module.exports = Product;
--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -1,28 +1,80 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
 
-// The `/api/categories` endpoint
-
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get("/", (req, res) => {
+  Category.findAll()
+    .then((dbCategory) => res.json(dbCategory))
+    .catch((err) => {
+      console.log(err), res.status(500).json(err);
+    });
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get("/:id", (req, res) => {
+  Category.findOne({
+    where: { id: req.params.id },
+    include: {
+      model: Product,
+      attributes: ["id", "product_name", "price"],
+    },
+  })
+    .then((dbCategory) => {
+      if (!dbCategory) {
+        res.status(404).json({ message: "No products found in this category!" });
+      }
+      res.json(dbCategory);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post("/", (req, res) => {
+  Category.create({
+    category_name: req.body.category_name,
+  })
+    .then((dbCategory) => res.json(dbCategory))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put("/:id", (req, res) => {
+  Category.update(
+    { category_name: req.body.category_name },
+    {
+      where: { id: req.params.id },
+    }
+  )
+    .then((dbCategory) => {
+      if (!dbCategory) {
+        res.status(404).json({ message: "The category doesnt exist!" });
+        return;
+      }
+      res.json(dbCategory);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+router.delete("/:id", (req, res) => {
+  Category.destroy({
+    where: { id: req.params.id },
+  })
+    .then((dbCategory) => {
+      if (!dbCategory) {
+        res.status(404).json({ message: "No category found with this id!" });
+        return;
+      }
+      res.json(dbCategory);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
